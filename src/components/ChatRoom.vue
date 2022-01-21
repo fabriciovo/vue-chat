2f01d652ebc9a304b895d3267374c9639b41f7ed
Fabricio Varisco <fabricio.v.o@hotmail.com>
2022-01-21 00:01:41 -0300

    vue gh pages

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -1,123 +1,132 @@
 <template>
-  <main>
-    <h3>Welcome to ChatRoom {{ chatId }}</h3>
+  <main class="section">
+    <h3>Welcome to ChatRoom.vue {{ chatId }}</h3>
+
+    <router-link to="/">Back</router-link>
+
     <p>
-      Open this link in another browser window to chat
+    Open this link in another browser window to chat 
       <code>https://your-url.com/#/chats/{{ chatId }}</code>
     </p>
 
-    <router-link to="/">Back</router-link>
     <User #user="{ user }">
       <div v-if="user">
         <ul>
           <li v-for="message of messages" :key="message.id">
-            <ChatMessage
-              :message="message"
-              :owner="user.uid === message.sender"
-            />
+            <ChatMessage :message="message" :owner="user.uid === message.sender" />
           </li>
         </ul>
-        <input v-model="newMessageText" />
+
         <hr />
         <h5>Record Audio</h5>
 
-        <button v-if="!recorder" @click="record()" class="button is-info">Record Audio</button>
+        <button v-if="!recorder" @click="record()" class="button is-info">Record Voice</button>
         <button v-else @click="stop()" class="button is-danger">Stop</button>
+ 
+        <br />
 
         <audio v-if="newAudio" :src="newAudioURL" controls></audio>
+
         <hr />
+
+        <input v-model="newMessageText" class="input" />
+
         <button
-          :disabled="!newMessageText || loading"
+          :disabled="(!newMessageText && !newAudio) || loading"
           class="button is-success"
           type="text"
-          @click="addMessage(user.uid, newMessageText)"
-        >
-          Send
-        </button>
+          @click="addMessage(user.uid)"
+        >Send</button>
       </div>
+
       <Login v-else />
     </User>
   </main>
 </template>
 
 <script>
+
+import User from './User.vue';
+import ChatMessage from './ChatMessage.vue';
 import Login from './Login.vue';
-import User from "./User.vue";
-import ChatMessage from "./ChatMessage.vue";
-import { db, storage } from "../firebase";
+import { db, storage } from '../firebase';
+
 export default {
   components: {
     User,
+    Login,
     ChatMessage,
-    Login
   },
   data() {
-    return {
-      newMessageText: "",
-      loading: false,
-      messages: [],
-      newAudio: null,
-      recorder: null,
-    };
+      return {
+          newMessageText: '',
+          loading: false,
+          messages: [],
+          newAudio: null,
+          recorder: null,
+      }
   },
   computed: {
     chatId() {
       return this.$route.params.id;
     },
     messagesCollection() {
-      return db.doc(`chats/${this.chatId}`).collection("messages");
+      return db.doc(`chats/${this.chatId}`).collection('messages');
     },
+    newAudioURL() {
+      return URL.createObjectURL(this.newAudio);
+    }
   },
   firestore() {
     return {
-      messages: this.messagesCollection.orderBy("createdAt").limitToLast(10),
+      messages: this.messagesCollection.orderBy('createdAt').limitToLast(10)
     };
-  },
-  newAudioURL() {
-    return URL.createObjectURL(this.newAudio);
   },
   methods: {
     async addMessage(uid) {
-      this.loading = true;
 
-      let audioURL = null;
+        this.loading = true;
 
-      const { id: messageId } = this.messagesCollection.doc();
+        let audioURL = null;
 
-      if (this.newAudio) {
-        const storageRef = storage
-          .ref("chats")
-          .child(this.chatId)
-          .child(`${messageId}.wav`);
+        const { id: messageId } = this.messagesCollection.doc();
 
-        await storageRef.put(this.newAudio).on();
+        if (this.newAudio) {
+          const storageRef = storage
+            .ref('chats')
+            .child(this.chatId)
+            .child(`${messageId}.wav`);
 
-        audioURL = await storageRef.getDownloadURL();
-      }
+            await storageRef.put(this.newAudio);
 
-      await this.messagesCollection.doc(messageId).set({
-        text: this.newMessageText,
-        sender: uid,
-        createdAt: Date.now(),
-        audioURL,
-      });
+          audioURL = await storageRef.getDownloadURL();
+        }
 
-      this.loading = false;
-      this.newMessageText = "";
-      this.audioURL = null;
+
+         await this.messagesCollection.doc(messageId).set({
+            text: this.newMessageText,
+            sender: uid,
+            createdAt: Date.now(),
+            audioURL
+        });
+
+        this.loading = false;
+        this.newMessageText = '';
+        this.newAudio = null;
     },
     async record() {
       this.newAudio = null;
 
       const stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
-        video: false,
+        video: false
       });
+
       const options = { mimeType: "audio/webm" };
       const recordedChunks = [];
       this.recorder = new MediaRecorder(stream, options);
 
-      this.recorder.addEventListener("dataavailable", (e) => {
+      this.recorder.addEventListener("dataavailable", e => {
         if (e.data.size > 0) {
           recordedChunks.push(e.data);
         }
@@ -133,10 +142,11 @@ export default {
     async stop() {
       this.recorder.stop();
       this.recorder = null;
-    },
-  },
+    }
+  }
 };
 </script>
+
 
 <style scoped>
 ul {
