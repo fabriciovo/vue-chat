e0511702c3dd8be6de1ab14dfa35738d710e0dc4
Fabricio Varisco <fabricio.v.o@hotmail.com>
2022-01-24 14:51:20 -0300

    Fix user problems

--- src/components/Chats.vue
+++ src/components/Chats.vue
@@ -1,11 +1,12 @@
 <template>
   <User>
     <template #user="{ user }">
-      <v-row no-gutters>
-        {{user}}
-        <v-col md="3"><ChatList :uid="user.uid" /> </v-col>
-        <v-col cols="12" sm="6" md="8"><ChatRoom :user="user" /> </v-col>
-      </v-row>
+      <div v-if="user">
+        <v-row no-gutters>
+          <v-col md="3"><ChatList :uid="user.uid" /> </v-col>
+          <v-col cols="12" sm="6" md="8"><ChatRoom :user="user" /> </v-col>
+        </v-row>
+      </div>
     </template>
   </User>
 </template>
@@ -16,7 +17,6 @@ import ChatRoom from "./ChatRoom.vue";
 import User from "./User.vue";
 export default {
   components: { ChatList, ChatRoom, User },
-  props: ["user"],
 };
 </script>
 
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,56 +2,58 @@
   <div>
     <User>
       <template #user="{ user }">
-        <v-row justify="center">
-          <v-dialog v-model="dialog" persistent max-width="600px">
-            <v-card>
-              <v-card-title>
-                <span class="text-h5">User Profile</span>
-              </v-card-title>
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        label="Username"
-                        hint="Display Name"
-                        v-model="displayName"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="dialog = false">
-                  Close
-                </v-btn>
-                <v-btn color="blue darken-1" text @click="updateUserData()">
-                  Save
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-row>
-        <v-card class="mx-auto" max-width="344">
-          <v-img
-            class="avatar"
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            height="200px"
-            alt="Avatar"
-          ></v-img>
+        <div v-if="user">
+          <v-row justify="center">
+            <v-dialog v-model="dialog" persistent max-width="600px">
+              <v-card>
+                <v-card-title>
+                  <span class="text-h5">User Profile</span>
+                </v-card-title>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          label="Username"
+                          hint="Display Name"
+                          v-model="displayName"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="dialog = false">
+                    Close
+                  </v-btn>
+                  <v-btn color="blue darken-1" text @click="updateUserData()">
+                    Save
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-row>
+          <v-card class="mx-auto" max-width="344">
+            <v-img
+              class="avatar"
+              src="https://www.w3schools.com/howto/img_avatar.png"
+              height="200px"
+              alt="Avatar"
+            ></v-img>
 
-          <v-card-title> </v-card-title>
+            <v-card-title> </v-card-title>
 
-          <v-card-subtitle> {{ user.email }} </v-card-subtitle>
+            <v-card-subtitle> {{ user.email }} </v-card-subtitle>
 
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="orange lighten-2" text @click="dialog = true">
-              Edit
-            </v-btn>
-          </v-card-actions>
-        </v-card>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="orange lighten-2" text @click="dialog = true">
+                Edit
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
       </template>
     </User>
   </div>
